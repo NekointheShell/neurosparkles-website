1fe7822f67816925345636b7fbea570832da47db
neko <neko@127.0.0.1>
2023-04-17 00:15:15 -0400

    play with chart.js

---
+++ src/website/blueprints/metrics.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, request, session, redirect, url_for
+import website.helpers as helpers
+import website.errors as errors
+
+
+metrics = Blueprint('metrics', __name__)
+
+
+@metrics.route('/metrics', methods = ['GET', 'POST'])
+def metrics():
+    if request.method == 'GET':
+        return render_template('pages/metrics.html')
